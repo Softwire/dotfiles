031c973722af1e2cd999170afe6810daab93d246
Richard Bradley <Richard.Bradley@softwire.com>
2011-09-19 13:09:11 +0100

    set LANG no longer seems to be necessary

--- c/cygwin/cygwin.bat
+++ c/cygwin/cygwin.bat
@@ -5,13 +5,15 @@ chdir %HOME%
 
 set PATH=C:\cygwin\bin;C:\cygwin\usr\X11R6\bin;%PATH%
 
+rem If you have char encoding issues, try this:
+rem
 rem # It might be better to use utf-8, which is the new cygwin default, but rxvt doesn't support it (!)
 rem # See http://omgili.com/mailinglist/cygwin/cygwin/com/4067829B4871054DB7E77A511FCE9224013E3E5Bexch-empcampusgeorgefoxedu.html
-
+rem
+rem Previously I had set LANG=en_UK.Windows-1252
+rem and set LC_CTYPE=C
+rem
 rem # you may want to set PERL_BADLANG=0
-
-set LANG=en_UK.Windows-1252
-set LC_CTYPE=C
 
 rem extra options didn't use to be necessary!
 rem old = rxvt -sl 2000 -sr -title bash -e bash --login -i
